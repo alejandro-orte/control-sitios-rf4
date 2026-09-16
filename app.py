113f033388bdfc6270aac224d8cfad52c5e79c9c
alejandro-orte <69557357+alejandro-orte@users.noreply.github.com>
2026-09-16 08:48:32 -0500

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ st.set_page_config(
     page_title="Control Semanal de Sitios BSS", page_icon="📡", layout="wide"
 )
 
-st.title("📡 Tablero de Control de Sitios BSS")
+st.title("📡 Tablero de Control de Sitios")
 
 # ==========================================
 # CONFIGURACIÓN DE URLS Y CONTRASEÑA
@@ -52,14 +52,14 @@ def modal_autenticacion():
 
 # Botón en la barra lateral que activa el modal
 st.sidebar.header("🔄 Sincronización")
-if st.sidebar.button("Actualizar datos desde Google Sheets"):
+if st.sidebar.button("Actualizar datos"):
   modal_autenticacion()
 
 # ==========================================
 # CREACIÓN DE PESTAÑAS
 # ==========================================
 tab_general, tab_rechazados = st.tabs(
-    ["📋 General BSS", "🚫 Sitios Rechazados (Umbrella)"]
+    ["📋 General", "🚫 Sitios Rechazados (Umbrella)"]
 )
 
 
@@ -68,7 +68,7 @@ tab_general, tab_rechazados = st.tabs(
 # ==========================================
 with tab_general:
   st.write(
-      "Sincronización en tiempo real desde **Google Sheets** (Excluyendo"
+      "Sincronización en tiempo real (Excluyendo"
       " sitios en **PRODUCCIÓN**)."
   )
 
